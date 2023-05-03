4380e2d50c48a8af6852da99d2b87ea52e3540f6
s3rverdenied <78494964+s3rverdenied@users.noreply.github.com>
2023-05-03 15:03:00 +0700

    Add files via upload

---
+++ biodata.js
@@ -0,0 +1,20 @@
+const biodata = {
+  name: 'Abubakar Adeni',
+  age: 23,
+  hobbies: ['Menonton Film', 'Berenang', 'Kuliner'],
+  isMarried: false,
+  schoolList: [
+    { name: 'Universitas Nusa Mandiri', yearIn: 2018, yearOut: 2023, major: 'Sarjana Sistem Informasi' },
+    { name: 'SMAN 19 Jakarta', yearIn: 2019, yearOut: 2018, major: null },
+    { name: 'SMPN 63 Jakarta', yearIn: 2010, yearOut: 2013, major: null },
+    { name: 'SDN Pekojan 03', yearIn: 2006, yearOut: 2012, major: null }
+  ],
+  skills: [
+    { skillName: 'JavaScript', level: 'beginner' },
+    { skillName: 'HTML & CSS', level: 'advanced' },
+    { skillName: 'Node.js', level: 'beginner' },
+    { skillName: 'SQL', level: 'advanced' },
+    { skillName: 'Bootstrap', level: 'advanced' }
+  ],
+  interestInCoding: true
+};
